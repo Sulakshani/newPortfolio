e1a2ec42fdeb722e876930b7d0006091559905dd
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2025-07-30 23:17:04 +0530

    Initial commit: Next.js portfolio setup with Tailwind CSS

---
+++ myportfolio/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"]});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sulakshani's Portfolio",
+  description: "Modern portfolio showcasing projects and skills",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black-100`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ myportfolio/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main className="relative bg-black-100">
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Explore my projects and skills.</p>
+    </div>
+   </main>
+  );
+}
